5e4442e1627c23f1f38e9de9413bd0562362817e
Edel Benavides <edelbenavides@gmail.com>
2018-12-15 00:57:22 -0500

    fixed prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
   auth: {
     CLIENT_ID: 'Eg_pghBkf0aAc_KMXODOAmTXK096rcZO',
     CLIENT_DOMAIN: 'edelphotography.auth0.com', // e.g., 'you.auth0.com'
-    REDIRECT: 'http://edelphotography.com',
-    LOGOUT_URL: 'http://edelphotography.com'
+    REDIRECT: 'https://edelphotography.com',
+    LOGOUT_URL: 'https://edelphotography.com'
   },
   endpoints: {
     flickr: 'http://3.17.43.124:8081/'
